4aca4e9233cf543ca99e8098326333b02d2011d8
Richard Elms <richardelms@gmail.com>
2023-10-12 15:39:19 +0200

    logging

--- features/scripts/build_ios_app.sh
+++ features/scripts/build_ios_app.sh
@@ -9,6 +9,8 @@ echo "Flutter Bin: $FLUTTER_BIN"
 
 EXPORT_OPTIONS="$(pwd)/features/fixture_resources/exportOptions.plist"
 
+echo "EXPORT OPTIONS: $EXPORT_OPTIONS"
+
 cd features/fixtures/mazerunner
 
 $FLUTTER_BIN build ipa --export-options-plist=$EXPORT_OPTIONS --no-tree-shake-icons
