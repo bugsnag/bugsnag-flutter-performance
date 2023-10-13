b7de4cbfe4aa2f9d96641201d52dc5f6e94ca370
Richard Elms <richardelms@gmail.com>
2023-10-13 14:18:44 +0200

    ttt

--- features/scripts/build_ios_app.sh
+++ features/scripts/build_ios_app.sh
@@ -5,12 +5,16 @@ if [ -z "$FLUTTER_BIN" ]; then
   FLUTTER_BIN="flutter"
 fi
 
-echo "Flutter Bin: $FLUTTER_BIN"
-
 EXPORT_OPTIONS="$(pwd)/features/fixture_resources/exportOptions.plist"
-
-echo "EXPORT OPTIONS: $EXPORT_OPTIONS"
 
 cd features/fixtures/mazerunner/ios
 
 $FLUTTER_BIN build ipa --export-options-plist=$EXPORT_OPTIONS --no-tree-shake-icons
+
+ls
+
+cd ..
+
+cd build/ios
+
+ls
