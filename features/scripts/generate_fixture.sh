100eb36ea5724fc8e97f334c70fb6422406bdda2
Richard Elms <richardelms@gmail.com>
2023-10-13 11:49:09 +0200

    sed magic

--- features/scripts/generate_fixture.sh
+++ features/scripts/generate_fixture.sh
@@ -13,20 +13,18 @@ EXPORT_OPTIONS="features/fixture_resources/exportOptions.plist"
 
 EXPORT_OPTIONS_DEST="features/fixtures/flutterperformancefixture/ios/exportOptions.plist"
 
+XCODE_PROJECT=features/fixtures/mazerunner/ios/Runner.xcodeproj/project.pbxproj
+
 echo "Remove old fixture"
 
 rm -rf $FIXTURE_LOCATION
 
 echo "Create blank fixture"
 
-$FLUTTER_BIN create $FIXTURE_LOCATION  --org com.bugsnag
+$FLUTTER_BIN create $FIXTURE_LOCATION  --org com.bugsnag --platforms=ios,android
 
 echo "Add perf sdk dep"
 
 $FLUTTER_BIN pub add --directory="$FIXTURE_LOCATION" "bugsnag_flutter_performance:{'path':'$PACKAGE_PATH'}"
 
-# echo "Move Ios Export Options"
-
-# cp $EXPORT_OPTIONS $EXPORT_OPTIONS_DEST
-
-echo "Move Fixture Code"
+sed -i '' "s/ENABLE_BITCODE = NO;/ENABLE_BITCODE = NO;\nDEVELOPMENT_TEAM = 7W9PZ27Y5F;/g" "$XCODE_PROJECT"
