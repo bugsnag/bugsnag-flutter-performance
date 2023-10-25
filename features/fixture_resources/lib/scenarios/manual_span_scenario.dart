50b34c29f8f09b792ec80f2b793c32953829178a
Richard Elms <richardelms@gmail.com>
2023-10-25 15:27:10 +0200

    using mazerunner logs

--- features/fixture_resources/lib/scenarios/manual_span_scenario.dart
+++ features/fixture_resources/lib/scenarios/manual_span_scenario.dart
@@ -1,18 +1,19 @@
 
+import 'dart:convert';
+
 import 'package:bugsnag_flutter_performance/bugsnag_flutter_performance.dart';
 import 'package:mazerunner/main.dart';
 
 import 'scenario.dart';
+import 'package:http/http.dart' as http;
 
 class ManualSpan extends Scenario {
   @override
   Future<void> run() async {
     log("running manual span scenario");
-    const apiKey = '0123456789abcdef0123456789abcdef';
-    BugsnagPerformanceClient client = BugsnagPerformanceClient();
-    log("using MAZE_HOST: " + FixtureConfig.MAZE_HOST.toString());
-    client.start(apiKey: apiKey, endpoint: Uri.parse("https://webhook.site/2c429399-3f1e-4c44-b4dd-59853fd1ff79"));
-    client.setBatchSize(1);
-    client.startSpan('Test').end();
+    http.post(Uri.parse(FixtureConfig.MAZE_HOST.toString() + "/logs"), body: jsonEncode({
+      "message": "manual span scenario"
+    }));
   }
 }
+
