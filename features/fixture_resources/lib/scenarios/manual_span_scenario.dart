ee2112d8f9e10d9ddfe0b05a77b1fbcc45ce8dcb
Richard Elms <richardelms@gmail.com>
2023-10-25 14:22:10 +0200

    logging

--- features/fixture_resources/lib/scenarios/manual_span_scenario.dart
+++ features/fixture_resources/lib/scenarios/manual_span_scenario.dart
@@ -7,8 +7,10 @@ import 'scenario.dart';
 class ManualSpan extends Scenario {
   @override
   Future<void> run() async {
+    log("running manual span scenario");
     const apiKey = '0123456789abcdef0123456789abcdef';
     BugsnagPerformanceClient client = BugsnagPerformanceClient();
+    log("using MAZE_HOST: " + FixtureConfig.MAZE_HOST.toString());
     client.start(apiKey: apiKey, endpoint: FixtureConfig.MAZE_HOST);
     client.setBatchSize(1);
     client.startSpan('Test').end();
