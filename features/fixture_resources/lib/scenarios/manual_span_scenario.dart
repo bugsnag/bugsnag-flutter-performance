b619543057ca5eda16f4e41e74bad981db7f7cf1
Richard Elms <richardelms@gmail.com>
2023-10-27 14:14:33 +0200

    manual url for debugging

--- features/fixture_resources/lib/scenarios/manual_span_scenario.dart
+++ features/fixture_resources/lib/scenarios/manual_span_scenario.dart
@@ -9,7 +9,7 @@ import 'package:http/http.dart' as http;
 class ManualSpanScenario extends Scenario {
   @override
   Future<void> run() async {
-    bugsnagPerformance.start(apiKey: '12312312312312312312312312312312', endpoint: FixtureConfig.MAZE_HOST);
+    bugsnagPerformance.start(apiKey: '12312312312312312312312312312312', endpoint: Uri.parse("https://webhook.site/9d5c4637-681b-426c-b52d-5440f83f6472"));
     bugsnagPerformance.setBatchSize(1);
     final span = bugsnagPerformance.startSpan('ManualSpanScenario');
     span.end();
