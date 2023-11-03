48bd8f973b78b25a601476f6d819f1183387a30c
Richard Elms <richardelms@gmail.com>
2023-11-03 09:46:39 +0100

    added e2e tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,6 +8,15 @@ Before('@skip_android') do |_scenario|
   skip_this_scenario('Not compatible with Android') if Maze::Helper.get_current_platform == 'android'
 end
 
+
+Before('@android_only') do |_scenario|
+  skip_this_scenario('Skipping scenario') unless Maze::Helper.get_current_platform == 'android'
+end
+
 Before('@skip_ios') do |_scenario|
   skip_this_scenario('Not compatible with iOS') if Maze::Helper.get_current_platform == 'ios'
+end
+
+Before('@ios_only') do |_scenario|
+  skip_this_scenario('Skipping scenario') unless Maze::Helper.get_current_platform == 'ios'
 end
