bbe4e00e085cce7e55c1ce5d5927a37c68164ffa
Richard Elms <richardelms@gmail.com>
2023-11-20 14:17:44 +0100

    remove unnecessary id part

--- packages/bugsnag_flutter_performance/lib/src/uploader/retry_queue.dart
+++ packages/bugsnag_flutter_performance/lib/src/uploader/retry_queue.dart
@@ -3,35 +3,30 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
 import 'package:bugsnag_flutter_performance/src/uploader/uploader.dart';
-import 'package:uuid/uuid.dart';
 
 import 'model/otlp_package.dart';
 
 class CachedPayloadModel {
-  String id;
   Map<String, String> headers;
   Uint8List body;
 
-  CachedPayloadModel({required this.id, required this.headers, required this.body});
+  CachedPayloadModel({required this.headers, required this.body});
 
   factory CachedPayloadModel.fromJson(Map<String, dynamic> json) {
     dynamic headersJson = json['headers'];
     Map<String, String> headers = Map<String, String>.from(
-          headersJson.map((key, value) => MapEntry<String, String>(key, value.toString())));
+        headersJson.map((key, value) => MapEntry<String, String>(key, value.toString())));
 
     return CachedPayloadModel(
-      id: json['id'],
       headers: headers,
       body: base64Decode(json['body']),
     );
-
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'headers': headers, 'body': base64Encode(body)};
+    return {'headers': headers, 'body': base64Encode(body)};
   }
 }
-
 
 abstract class RetryQueue {
   Future<void> enqueue({
@@ -46,7 +41,6 @@ class FileRetryQueue implements RetryQueue {
   static const String _cacheDirectoryName = 'bugsnag-performance/v1/batches';
   static const Duration _maxAge = Duration(hours: 24);
 
-  final Uuid _uuid = const Uuid();
   final Uploader? _uploader;
 
   FileRetryQueue(Uploader uploader) : _uploader = uploader;
@@ -56,9 +50,8 @@ class FileRetryQueue implements RetryQueue {
     required Map<String, String> headers,
     required Uint8List body,
   }) async {
-    final id = _uuid.v4();
-    final payload = jsonEncode(CachedPayloadModel(id: id, headers: headers, body: body));
-    final fileName = _generateFileName(id);
+    final payload = jsonEncode(CachedPayloadModel(headers: headers, body: body));
+    final fileName = _generateFileName();
     await _writeToFile(fileName, payload);
   }
 
@@ -80,8 +73,7 @@ class FileRetryQueue implements RetryQueue {
         final payload = await file.readAsString();
         final cachedPayloadModel = CachedPayloadModel.fromJson(jsonDecode(payload));
         final success = await _sendPayload(cachedPayloadModel);
-        if(success)
-        {
+        if (success) {
           file.deleteSync();
         }
       }
@@ -89,13 +81,12 @@ class FileRetryQueue implements RetryQueue {
   }
 
   Future<bool> _sendPayload(CachedPayloadModel payloadModel) async {
-      final package = OtlpPackage(
-        headers: payloadModel.headers,
-        payload: payloadModel.body,
-      );
-      final result = await _uploader?.upload(package: package);
-      return result == RequestResult.success;
-    }
+    final package = OtlpPackage(
+      headers: payloadModel.headers,
+      payload: payloadModel.body,
+    );
+    final result = await _uploader?.upload(package: package);
+    return result == RequestResult.success;
   }
 
   Future<void> _writeToFile(String fileName, String payload) async {
@@ -108,8 +99,9 @@ class FileRetryQueue implements RetryQueue {
     await file.writeAsString(payload);
   }
 
-  String _generateFileName(String id) {
-    return 'payload_$id.json';
+  String _generateFileName() {
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    return 'payload_$timestamp.json';
   }
 
   Future<Directory> _getCacheDirectory() async {
