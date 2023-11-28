4d2d6058dc6c1dc260edfc0a67018a6313c7c16a
Richard Elms <richardelms@gmail.com>
2023-11-28 13:30:39 +0100

    add proxy to fixture generation

--- features/scripts/generate_fixture.sh
+++ features/scripts/generate_fixture.sh
@@ -39,6 +39,8 @@ $FLUTTER_BIN pub add --directory="$FIXTURE_LOCATION" path_provider
 
 $FLUTTER_BIN pub add --directory="$FIXTURE_LOCATION" http
 
+$FLUTTER_BIN pub add --directory="$FIXTURE_LOCATION" native_flutter_proxy
+
 echo "Add dev team to Xcode project"
 
 sed -i '' "s/ENABLE_BITCODE = NO;/ENABLE_BITCODE = NO;\nDEVELOPMENT_TEAM = 7W9PZ27Y5F;\nCODE_SIGN_STYLE = Automatic;/g" "$XCODE_PROJECT"
