342ad22e1ac8ab4fe20e74f27dd383d414d626c3
Robert <robert.smartbear@gmail.com>
2024-04-10 23:43:53 +0200

    E2E tests fixture changes

---
+++ features/fixture_resources/lib/scenarios/manual_navigation_span_scenario.dart
@@ -0,0 +1,17 @@
+import 'package:bugsnag_flutter_performance/bugsnag_flutter_performance.dart';
+import 'scenario.dart';
+
+class ManualNavigationSpanScenario extends Scenario {
+  @override
+  Future<void> run() async {
+    await startBugsnag();
+    setMaxBatchSize(1);
+    bugsnag_performance
+        .startNavigationSpan(
+          routeName: 'navigationScenarioRoute',
+          navigatorName: 'customNavigator',
+          previousRoute: 'navigationScenarioPreviousRoute',
+        )
+        .end();
+  }
+}
