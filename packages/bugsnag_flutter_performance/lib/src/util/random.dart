c5358959a3fd8e2991d7c142d0bc5778997d5fa2
Robert <robert.smartbear@gmail.com>
2024-09-18 15:53:18 +0200

    Ensure traceIds are generated with most secure random generator available

--- packages/bugsnag_flutter_performance/lib/src/util/random.dart
+++ packages/bugsnag_flutter_performance/lib/src/util/random.dart
@@ -1,15 +1,15 @@
 import 'dart:math';
 
 BigInt randomSpanId() {
-  return randomValue(8);
+  return randomValue(8, false);
 }
 
 BigInt randomTraceId() {
-  return randomValue(16);
+  return randomValue(16, true);
 }
 
-BigInt randomValue(int length) {
-  final random = _Random();
+BigInt randomValue(int length, bool secure) {
+  final random = _Random(secure);
   BigInt result = BigInt.from(random.nextInt(256) & 0xff);
   for (var i = 1; i < length; i++) {
     int byte = random.nextInt(256);
@@ -19,14 +19,17 @@ BigInt randomValue(int length) {
 }
 
 class _Random {
-  static final _sharedRandom = Random();
+  static final _sharedRandom = _createSharedRandom();
   late final Random? _secureRandom;
+  final bool secure;
 
-  _Random() {
+  _Random(this.secure) {
     try {
-      _secureRandom = Random.secure();
-    } catch (e) {
-      // deliberately ignored
+      if (secure) {
+        _secureRandom = Random.secure();
+      }
+    } catch (_) {
+      // If the platform doesn't provide a secure random source then we fall back on a less secure version
     }
   }
 
@@ -35,9 +38,19 @@ class _Random {
       if (_secureRandom != null) {
         return _secureRandom!.nextInt(max);
       }
-    } catch (e) {
-      // deliberately ignored
+    } catch (_) {
+      // When the system runs out of entropy, we fall back on a less secure random number source
+      // Although the documentation doesn't mention it, nextInt can throw
     }
     return _sharedRandom.nextInt(max);
+  }
+
+  static Random _createSharedRandom() {
+    try {
+      final secureRandom = Random.secure();
+      return Random(secureRandom.nextInt(1 << 32));
+    } catch (_) {
+      return Random();
+    }
   }
 }
